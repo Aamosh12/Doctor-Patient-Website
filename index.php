2e91c5f84a6f33633174900f895c624114426dee
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-06-26 18:17:00 +0545

    feat: Add Content in index.php

--- index.php
+++ index.php
@@ -5,15 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CareConnect</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="./style/index.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/font-awesome.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://fonts.googleapis.com/css? family=kaushan+script|popping&display=swap" rel="stylesheet"/>
 </head>
 </head>
 
 <body>
-    <div class="full-page" id="main">
+    <div class="container-fluid" id="main">
         <div id="background"></div>
         <header>
             <div id="nav">
@@ -22,15 +23,28 @@
                 ?>
             </div>
         </header>
-        <div id="content">
-            <?php 
-            include './index-content/login.php';
-            ?>
-        </div>
+        <section class="row">
+            <div id="content">
+                <?php
+                include './index-content/login.php';
+                ?>
+                <div class="col" id="main_content">
+                    <h1 id="content-title">CareConnect</h1>
+                    <h4>Connecting You to a Better World</h4>
+                    <button class="btn btn-outline-secondary" type="button" id="getting_started" style="color: #27374d;">Getting Started</button>
+                    <div class="social">
+                        <a href="https://www.facebook.com/"><i class="fa-sharp fa-solid fa-xmark" style="size: 30px;"></i></a>
+                        <a href=""><i class="fa-brands fa-twitter"></i></a>
+                        <a href="https://www.instagram.com/mhrznaamosh/"><i class="fa fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
     <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <script src="./scripts/login.js"></script>
+    <script src="https://kit.fontawesome.com/2c695f0152.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
