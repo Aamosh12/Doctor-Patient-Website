0dd8b76c388241db867a5a62485b9f65142f0397
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-06-26 18:18:17 +0545

    feat: Include script for closing login form

--- index-content/login.php
+++ index-content/login.php
@@ -1,7 +1,7 @@
 <div id="loginDiv">
     <div id="login-form">
-        <div id="xmark" onclick="document.getElementById('loginDiv').style.display='none'"></div>
-        <i class="fa-sharp fa-solid fa-xmark" id="cross" onclick="document.getElementById('loginDiv').style.display='none'"></i>
+        <div id="xmark" onclick="hideLogin()"></div>
+        <i class="fa-sharp fa-solid fa-xmark" id="cross" onclick="hideLogin()"></i>
         <div class="button-form">
             <div id="btn"></div>
             <button type="button" onclick="login()" class="top-btn">Log In</button>
@@ -19,3 +19,9 @@
         </div>
     </div>
 </div>
+<script>
+    function hideLogin(){
+    document.getElementById('loginDiv').style.display='none';
+    document.getElementById('main_content').style.display='block';
+    }
+</script>
