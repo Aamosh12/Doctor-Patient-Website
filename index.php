5d34c0f69ebd66aa7bc3cd19cb7fd644562ac426
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-06-30 13:14:10 +0545

    fix: resubmission error in refresh solve

--- index.php
+++ index.php
@@ -38,6 +38,12 @@
             </div>
         </section>
     </div>
+    <script>
+        if (window.history.replaceState)
+        {
+            window.history.replaceState(null, null, window.location.href);
+        }
+    </script>
     <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <script src="./scripts/login.js"></script>
