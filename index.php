4ed47f0d34aba155b3e493d75468a6cf1367141a
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-07-28 21:22:33 +0545

    fix: Change link for icons

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CareConnect</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="./style/index.css">
 </head>
 
@@ -30,9 +32,12 @@
                     <h4>Connecting You to a Better World</h4>
                     <button class="btn btn-outline-secondary" type="button" id="getting_started" style="color: #27374d;">Getting Started</button>
                     <div class="social">
-                        <a href="https://www.facebook.com/"><i class="fa-sharp fa-solid fa-xmark" style="size: 30px;"></i></a>
+                        <a href="https://www.facebook.com/">
+                        <i class="fa-brands fa-facebook-f"></i>
+                        </a>
+                        <br>
                         <a href=""><i class="fa-brands fa-twitter"></i></a>
-                        <a href="https://www.instagram.com/mhrznaamosh/"><i class="fa fa-instagram"></i></a>
+                        <a href="https://www.instagram.com/mhrznaamosh/"><i class="fa-brands fa-instagram"></i></a>
                     </div>
                 </div>
             </div>
