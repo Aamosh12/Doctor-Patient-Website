1e503d938c50f41f73433d212b34f171d9bdd226
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-07-31 00:55:43 +0545

    chore: replace Line Awesome with Font Awesome icons

--- pages/admdashboard.php
+++ pages/admdashboard.php
@@ -42,44 +42,40 @@ $result = $conn->query($sql_select);
     <title>Dashboard</title>
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <link rel="stylesheet" href="../style/admin_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 
 <body>
-
-    <div class="sidebar">
+<div class="sidebar">
         <div class="sidebar-brand">
             <h2><span class="lab la-Careconnect"></span>CareConnect</h2>
         </div>
         <div class="sidebar-menu">
             <ul>
                 <li>
-                    <a href="" class="active"><span class="las la-igloo"></span>
+                    <a href="" class="active"><i class="fa-solid fa-igloo"></i>
                         <span>Dashboard</span></a>
                 </li>
                 <li>
-                    <a href=""><span class="las la-users"></span>
+                    <a href="./admin/patient.php"><i class="fa-solid fa-user-group"></i>
                         <span>Patients</span></a>
                 </li>
-                <li><a href=""><span class="las la-users"></span>
+                <li><a href="./admin/doctor.php"><i class="fa-solid fa-user-doctor"></i>
                         <span>Doctors</span></a>
                 </li>
                 <li>
-                    <a href=""><span class="las la-file-invoice"></span>
+                    <a href=""><i class="fa-solid fa-file-invoice"></i>
                         <span>Reports</span></a>
                 </li>
-                <li class="dropdown">
-                    <a href="#"><span class="las la-user-circle"></span>
-                        <span>Accounts</span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Edit</a></li>
-                        <li><a href="#">Log Out</a></li>
-                    </ul>
+                <li>
+                    <a href="./admin/logout.php"><i class="fa-solid fa-right-from-bracket"></i> <span>Log Out</span></a>
                 </li>
             </ul>
         </div>
     </div>
     <div class="main-content">
-        <header>
+    <header>
             <h3>
                 <label for="">
                     <span class="las la-bars"></span>
