bf93b792a7548c2c6cfdf1bf6932d9a96089ecf8
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-07-31 01:00:08 +0545

    feat: make separate sidebar.php for reusability of sidebar

---
+++ pages/admin/sidebar.php
@@ -0,0 +1,32 @@
+<div class="sidebar">
+        <div class="sidebar-brand">
+            <h2><span class="lab la-Careconnect"></span>CareConnect</h2>
+        </div>
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <a href="" class="active"><span class="las la-igloo"></span>
+                        <span>Dashboard</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-users"></span>
+                        <span>Patients</span></a>
+                </li>
+                <li><a href=""><span class="las la-users"></span>
+                        <span>Doctors</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-file-invoice"></span>
+                        <span>Reports</span></a>
+                </li>
+                <li class="dropdown">
+                    <a href="#"><span class="las la-user-circle"></span>
+                        <span>Accounts</span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Edit</a></li>
+                        <li><a href="#">Log Out</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
