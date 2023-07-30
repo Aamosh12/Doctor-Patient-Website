a9dbe6aedd766312e0f8515519174a90a9a0e62c
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-07-31 00:58:32 +0545

    feat: Made separate header.php for reusability

---
+++ pages/admin/header.php
@@ -0,0 +1,13 @@
+<header>
+            <h3>
+                <label for="">
+                    <span class="las la-bars"></span>
+                </label>
+                Dashboard
+            </h3>
+            <div class="user">
+                <div>
+                    <h4>User Admin</h4>
+                </div>
+            </div>
+        </header>
