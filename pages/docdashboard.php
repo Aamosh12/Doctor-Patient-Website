9eba306b99b85d0905f448ed0fa6288dc9ed1150
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-08-01 21:58:13 +0545

    new: update a new dashboard for doctor

--- pages/docdashboard.php
+++ pages/docdashboard.php
@@ -1,11 +1,58 @@
+<?php
+session_start();
+require '../actions/Connection.php';
+if (!isset($_SESSION["role"])) {
+    header("location: ../index.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
- <html lang="en">
- <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Doctor - CareConnect</title>
- </head>
- <body>
-        <h1>Welcome Doctor</h1>
- </body>
- </html>
+<html lang="en">
+
+<head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>CareConnect</title>
+       <link rel="stylesheet" href="../style/doctorDash.css">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+
+<body>
+       <div id="wrapper">
+              <div class="sidebar">
+                     <div class="sidebar-brand">
+                            <h2>CareConnect</h2>
+                     </div>
+                     <hr>
+                     <div class="sidebar-menu">
+                            <ul>
+                                   <li>
+                                          <a href="./docdashboard.php" class="active"><i class="fa-solid fa-house"></i>
+                                                 <span>Dashboard</span></a>
+                                   </li>
+                                   <li><a href="./doctor/patientRequest.php"><i class="fa-solid fa-user-doctor"></i>
+                                                 <span>Patient's Request</span></a>
+                                   </li>
+                                   <li>
+                                          <a href=""><i class="fa-solid fa-book-open-reader"></i>
+                                                 <span>Appointments</span></a>
+                                   </li>
+                                   <li>
+                                          <a href="./patient/logout.php"><i class="fa-solid fa-right-from-bracket"></i> <span>Log Out</span></a>
+                                   </li>
+                            </ul>
+                     </div>
+              </div>
+              <div class="main-content">
+                     <?php
+                     include './patient/header.php';
+                     ?>
+                     <main>
+                          
+                     </main>
+              </div>
+       </div>
+</body>
+
+</html>
