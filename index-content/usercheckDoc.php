ab564e21be42965077831067016a2877179bc04d
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-08-03 23:17:02 +0545

    feat: Add file to check user name in database

--- index-content/usercheckDoc.php
+++ index-content/usercheckDoc.php
@@ -1,7 +1,7 @@
 <?php
 require '../actions/Connection.php';
 
-$username = "SELECT * FROM user WHERE username='$_GET[username]' AND Role_id=2";
+$username = "SELECT * FROM user WHERE username='$_GET[username]' AND Role_id=1";
 $check = $conn->query($username);
 if($check->num_rows >0)
 {
