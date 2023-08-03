00cfa7aaa3b4992224070f29f5de0b9edbf8f423
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-08-03 23:16:36 +0545

    remove validation from register and include it in another file

--- index-content/registration.php
+++ index-content/registration.php
@@ -1,47 +1,3 @@
-<?php
-require './actions/Connection.php';
-date_default_timezone_set('Asia/Kathmandu'); // Set the time zone to Nepal (Asia/Kathmandu)
-$currentDateTime = date('Y-m-d H:i:s');
-
-if (isset($_POST['register-doctor'])) {
-    $name = $_POST['dname'];
-    $email = $_POST['demail'];
-    $address = $_POST['daddress'];
-    $phone = $_POST['dphone'];
-    $username = $_POST['dusername'];
-    $degree = $_POST['degree'];
-    $specialization = $_POST['spec'];
-    $nmc = $_POST['nmc'];
-    $experience = $_POST['experience'];
-    $password = $_POST['dpassword'];
-    $dcpassword = $_POST['dcpassword'];
-    $isverified = false;
-    
-    $user_sql = "INSERT INTO user VALUES ('', '$name', '$email', '$address', '$username', '$password', '$phone', '1', '$currentDateTime')";
-
-    if ($conn->query($user_sql) === TRUE) {
-        $select = "SELECT id FROM user WHERE Username='$username'";
-        $userid= $conn->query($select)->fetch_assoc()['id'];
-        $sql = "INSERT INTO doctor VALUES ('$userid','$degree', '$specialization', '$nmc', '$experience', '$isverified')";
-        if(!$conn->query($sql)){
-            die('Error: '.$conn->$error);
-        }
-    } else {
-        echo "Error: " . $sql . "<br>";
-    } 
-    $conn->close();
-}
-if (isset($_POST['register-patient'])) {
-    $name = $_POST['pname'];
-    $email = $_POST['pemail'];
-    $address = $_POST['paddress'];
-    $phone = $_POST['pnum'];
-    $username = $_POST['pusername'];
-    $password = $_POST['ppassword'];
-    $user_sql = "INSERT INTO user VALUES ('', '$name', '$email', '$address', '$username', '$password', '$phone', '2', '$currentDateTime')";
-    $conn->query($user_sql);
-}
-?>
 <div action="" id="register" class="input-group-register" method="post">
     <div id="choice">
         <h5>I am registering as:</h5>
@@ -54,12 +10,13 @@ if (isset($_POST['register-patient'])) {
     </div><br>
     <div id="scroll-form">
         <div id="field_doctor">
-            <form action="" id="register0" class="input-group-register0" method="post" onsubmit="return validatedoc()">
+            <form action="./index-content/register.php" id="register0" class="input-group-register0" method="post" onsubmit="return validatedoc()">
                 <input type="text" class="input-field" placeholder="Name" name="dname" required>
-                <input type="email" class="input-field" placeholder="Email" name="demail" required>
+                <input type="email" class="input-field" placeholder="Email" name="demail" id="email" required>
                 <input type="text" class="input-field" placeholder="Address" name="daddress" required>
                 <input type="tel" class="input-field" placeholder="Phone Number" name="dphone" required>
-                <input type="text" class="input-field" placeholder="Username" name="dusername" required>
+                <input type="text" class="input-field" placeholder="Username" name="dusername" onkeyup="httprequesdoc(this.value)" required>
+                <p id="userCheckdoc"></p>
                 <input type="text" class="input-field" placeholder="Your Degree Here" name="degree" required>
                 <input type="text" class="input-field" placeholder="Specialization" name="spec" required>
                 <input type="tel" class="input-field" placeholder="NMC No." name="nmc" required>
@@ -71,6 +28,7 @@ if (isset($_POST['register-patient'])) {
                 <div class="message-box" style="display: none;">
                     <p id="message">Registration Success</p>
                 </div>
+                <p id="emailExistMessage" style="display: none; color: red;">Email already exists in the database.</p>
                 <button type="submit" class="submit-btn" name="register-doctor" value="register-doctor">Register</button>
             </form>
         </div>
@@ -79,9 +37,9 @@ if (isset($_POST['register-patient'])) {
             if doctor don't validate client and vice versa
          -->
         <div id="field_patient">
-            <form action="" id="register2" class="input-group-register2" method="post" onsubmit="return validatepat()">
+            <form action="./index-content/registerpat.php" id="register2" class="input-group-register2" method="post" onsubmit="return validatepat()">
                 <input type="text" class="input-field" placeholder="Name" name="pname" required>
-                <input type="email" class="input-field" placeholder="Email" name="pemail" required>
+                <input type="email" class="input-field" placeholder="Email" name="pemail" id="email2" required>
                 <input type="text" class="input-field" placeholder="Address" name="paddress" required>
                 <input type="tel" class="input-field" placeholder="Phone No" name="pnum" required>
                 <input type="text" class="input-field" placeholder="Choose a valid user name" name="pusername" onkeyup="httpreques(this.value)" required>
@@ -90,12 +48,77 @@ if (isset($_POST['register-patient'])) {
                 <input type="password" class="input-field" placeholder="Confirm Password" name="pcpassword" id="pcpassword" required>
                 <p id="password_match_error_patient" class="text-danger"></p>
                 <input type="checkbox" class="check-box" required><span id="agreeText">I agree to the terms and conditions.</span>
+                <p id="emailExistMessage1" style="display: none; color: red;">Email already exists in the database.</p>
                 <button type="submit" class="submit-btn" name="register-patient">Register</button>
             </form>
         </div>
     </div>
 </div>
 <script>
+    // document.getElementById('register0').addEventListener('submit', function(event) {
+    //     event.preventDefault(); // Prevent the form from submitting normally
+
+    //     // Get the email value from the form
+    //     var email = document.getElementById('email').value;
+
+    //     // Create a new XMLHttpRequest object (AJAX request)
+    //     var xhr = new XMLHttpRequest();
+
+    //     // Configure the AJAX request
+    //     xhr.open('POST', './index-content/check_email.php', true);
+    //     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    //     // Define a callback function to handle the AJAX response
+    //     xhr.onreadystatechange = function() {
+    //         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    //             var response = xhr.responseText;
+    //             var emailExistMessage = document.getElementById('emailExistMessage');
+    //             if (response === 'exist') {
+    //                 // Show the email exist message
+    //                 emailExistMessage.style.display = 'block';
+    //             } else {
+    //                 // Proceed with form submission
+    //                 emailExistMessage.style.display = 'none'; // Hide the message if previously shown
+    //                 document.getElementById('register0').submit();
+    //             }
+    //         }
+    //     };
+
+    //     // Send the AJAX request with the email data
+    //     xhr.send('email=' + encodeURIComponent(email));
+    // });
+    // document.getElementById('register2').addEventListener('submit', function(event) {
+    //     event.preventDefault(); // Prevent the form from submitting normally
+
+    //     // Get the email value from the form
+    //     var email = document.getElementById('email2').value;
+
+    //     // Create a new XMLHttpRequest object (AJAX request)
+    //     var xhr = new XMLHttpRequest();
+
+    //     // Configure the AJAX request
+    //     xhr.open('POST', './index-content/check_emailPat.php', true);
+    //     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    //     // Define a callback function to handle the AJAX response
+    //     xhr.onreadystatechange = function() {
+    //         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    //             var response = xhr.responseText;
+    //             var emailExistMessage = document.getElementById('emailExistMessage1');
+    //             if (response === 'exist') {
+    //                 // Show the email exist message
+    //                 emailExistMessage.style.display = 'block';
+    //             } else {
+    //                 // Proceed with form submission
+    //                 emailExistMessage.style.display = 'none'; // Hide the message if previously shown
+    //                 document.getElementById('register2').submit();
+    //             }
+    //         }
+    //     };
+
+    //     // Send the AJAX request with the email data
+    //     xhr.send('email=' + encodeURIComponent(email));
+    // });
      function validatedoc() {
       var password = document.getElementById("dpassword").value;
       var confirmPassword = document.getElementById("dcpassword").value;
@@ -129,6 +152,20 @@ if (isset($_POST['register-patient'])) {
                 if(request.status == 200 && request.readyState == 4)
                 {
                     document.getElementById('userCheck').textContent = request.response;
+                }
+            }
+
+            request.send();
+    }
+    function httprequesdoc(user){
+        let request = new XMLHttpRequest();
+
+            request.open('GET', './index-content/usercheckDoc.php?username='+user);
+            
+            request.onreadystatechange = function () {
+                if(request.status == 200 && request.readyState == 4)
+                {
+                    document.getElementById('userCheckdoc').textContent = request.response;
                 }
             }
 
