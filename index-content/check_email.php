4c8d9c3cc8657c551a5744e15d29739532b274e0
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-08-03 23:14:02 +0545

    feat: Add PHP file to check email in the database

---
+++ index-content/check_email.php
@@ -0,0 +1,21 @@
+<?php
+require '../actions/Connection.php';
+$email = $_POST['email'];
+
+// Prepare the SQL query to check if the email already exists in the database
+$email = $conn->real_escape_string($email);
+$sql = "SELECT Email FROM user WHERE Email = '$email' AND Role_id = 1";
+
+// Execute the query
+$result = $conn->query($sql);
+
+// Check if there is any result (if the email already exists)
+if ($result->num_rows > 0) {
+    echo 'exist';
+} else {
+    echo 'not_exist';
+}
+
+// Close the database connection
+$conn->close();
+?>
