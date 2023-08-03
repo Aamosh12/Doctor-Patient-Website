98b2d3fb646c00a7db424693ba41600fa6744821
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-08-03 23:18:55 +0545

    chore: Update patient dashboard

--- pages/patdashboard.php
+++ pages/patdashboard.php
@@ -2,8 +2,11 @@
 session_start();
 require '../actions/Connection.php';
 if (!isset($_SESSION["role"])) {
-    header("location: ../index.php");
-    exit();
+       header("location: ../index.php");
+       exit();
+} elseif ($_SESSION["role"] != 2) {
+       header("location: ../index.php");
+       exit();
 }
 ?>
 <!DOCTYPE html>
@@ -53,47 +56,56 @@ if (!isset($_SESSION["role"])) {
                      include './patient/header.php';
                      ?>
                      <main>
-                            <div class="cards">
-                                   <div class="card-single">
-                                          <div>
-                                                 <h1>1</h1>
-                                                 <span>Successful Appointments</span>
-                                          </div>
-                                   </div>
-                                   <div class="card-single">
-                                          <div>
-                                                 <h1>1</h1>
-                                                 <span>Appointments</span>
-                                          </div>
-                                   </div>
-                            </div><br>
-                            <div class="recent-grid">
-                                   <div class="doctor">
-                                          <div class="card">
-                                                 <div class="table">
-                                                        <div class="table_header">
-                                                               <h3>Recently Appointments</h3>
-                                                        </div>
-                                                        <div class="table_section">
-                                                               <table>
-                                                                      <thead>
-                                                                             <tr>
-                                                                                    <th>Appointment Id</th>
-                                                                                    <th>Patient</th>
-                                                                                    <th>Doctor</th>
-                                                                                    <th>Time</th>
-                                                                                    <th>Status</th>
-                                                                                    
-                                                                             </tr>
-                                                                      </thead>
-                                                                      <tbody>
+                            <div class="welcome-section">
+                                   <h1>Welcome, Patient!</h1>
+                                   <p class="motivational-quote">"Your health is your most valuable asset. Take care of it and cherish it."</p>
+                                   <p class="wishes">Wishing you better health and a speedy recovery!</p>
+                            </div>
 
-                                                                      </tbody>
-                                                               </table>
-                                                        </div>
-                                                 </div>
-                                          </div>
+                            <div class="health-tips-section">
+                                   <h2>Health Tips</h2>
+                                   <ul>
+                                          <li>Stay hydrated - drink plenty of water throughout the day.</li>
+                                          <li>Get regular exercise to keep your body and mind healthy.</li>
+                                          <li>Eat a balanced diet with plenty of fruits and vegetables.</li>
+                                          <li>Practice mindfulness and relaxation techniques to reduce stress.</li>
+                                   </ul>
+                            </div>
+
+                            <div class="wellness-goals-section">
+                                   <h2>Wellness Goals</h2>
+                                   <p>Track your wellness goals and progress here:</p>
+                                   <ul>
+                                          <li><label for="goal1">Exercise for at least 30 minutes every day.</label></li>
+                                          <li><label for="goal2">Eat at least 5 servings of fruits and vegetables daily.</label></li>
+                                          <li><label for="goal3">Meditate or practice deep breathing for 10 minutes daily.</label></li>
+                                   </ul>
+                            </div>
+                            <div class="mindfulness-corner">
+                                   <h2>Mindfulness Corner</h2>
+                                   <p>Take a few moments to practice mindfulness with this guided meditation script:</p>
+                                   <div class="meditation-script">
+                                          <p>Close your eyes and take a deep breath in...</p>
+                                          <p>Hold it for a moment, then exhale slowly...</p>
+                                          <p>Continue to breathe deeply and let go of any tension or stress...</p>
+                                          <p>Focus on the present moment and the sensation of your breath...</p>
+                                          <p>When your mind wanders, gently bring your attention back to your breath...</p>
+                                          <p>Take this time to be fully present and find inner peace...</p>
+                                          <p>When you are ready, open your eyes and carry this sense of calm with you...</p>
                                    </div>
+                            </div>
+
+                            <div class="healthy-recipes-section">
+                                   <h2>Healthy Recipes</h2>
+                                   <p>Try these delicious and nutritious recipes to support your well-being:</p>
+                                   <ul>
+                                          <li>Quinoa Salad with Avocado and Chickpeas</li>
+                                          <li>Grilled Salmon with Lemon and Dill</li>
+                                          <li>Veggie Stir-Fry with Tofu</li>
+                                          <li>Homemade Greek Yogurt Parfait with Berries</li>
+                                   </ul>
+                            </div>
+
                      </main>
               </div>
        </div>
