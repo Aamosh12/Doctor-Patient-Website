714a1263710e0e06c46443e95e7fcc40df9effe3
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-10-14 13:26:17 +0545

    feat: Implement login validation for patients and doctors In this commit, we have added functionality to the logincheck.php file to validate logins for both patients and doctors. It now displays a sweet alert with an error message if the login is invalid. This change enhances the user experience by providing better feedback during the login process.

--- logincheck.php
+++ logincheck.php
@@ -1,40 +1,89 @@
 <?php
 require './actions/Connection.php';
-
-    if(isset($_POST['doc-submit'])){
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        $sql = "SELECT * FROM user WHERE username='$username' AND password='$password' AND Role_id=1";
-        $user= $conn->query($sql);
-        if($user->num_rows > 0){
+if (isset($_POST['doc-submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if ($username == 'admin') {
+        $sql = "SELECT * FROM user WHERE username='$username' AND password='$password' AND Role_id=3";
+        $user = $conn->query($sql);
+        if ($user->num_rows > 0) {
             $user = $user->fetch_assoc();
             $_SESSION['id'] =  $user['Id'];
             $_SESSION['username'] = $user['Username'];
-            $_SESSION['role']= $user['Role_id'];
-            $_SESSION['name']=$user['Name'];
-            header('location: ./pages/docdashboard.php');
-        }
-        else
-        {echo "<script>alert('Not Valid')</script>";
+            $_SESSION['role'] = $user['Role_id'];
+            $_SESSION['name'] = $user['Name'];
+            header('location: ./pages/admdashboard.php');
+        } else {
+            echo "<script>
+            document.addEventListener('DOMContentLoaded', function() {
+                        Swal.fire({
+                        icon: 'error',
+                        title: 'Invalid username or password',
+                        text: 'Please check your credentials and try again.',
+                        confirmButtonText: 'OK', // Customize the button text
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            Swal.close();
+                        }
+                    });
+            });
+            </script>";
         }
     }
-    if(isset($_POST['pat-submit'])){
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+    $sql = "SELECT * FROM user WHERE username='$username' AND password='$password' AND Role_id=1";
+    $user = $conn->query($sql);
+    if ($user->num_rows > 0) {
+        $user = $user->fetch_assoc();
+        $_SESSION['id'] =  $user['Id'];
+        $_SESSION['username'] = $user['Username'];
+        $_SESSION['role'] = $user['Role_id'];
+        $_SESSION['name'] = $user['Name'];
+        header('location: ./pages/docdashboard.php');
+    } else {
+        echo "<script>
+        document.addEventListener('DOMContentLoaded', function() {
+                    Swal.fire({
+                    icon: 'error',
+                    title: 'Invalid username or password',
+                    text: 'Please check your credentials and try again.',
+                    confirmButtonText: 'OK', // Customize the button text
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        Swal.close();
+                    }
+                });
+        });
+        </script>";
+    }
+}
+if (isset($_POST['pat-submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-        $sql = "SELECT * FROM user WHERE username='$username' AND password='$password' AND Role_id=2";
-        $user= $conn->query($sql);
-        if($user->num_rows > 0){
-            $user = $user->fetch_assoc();
-            $_SESSION['id'] =  $user['Id'];
-            $_SESSION['username'] = $user['Username'];
-            $_SESSION['role']= $user['Role_id'];
-            $_SESSION['name']=$user['Name'];
-            header('location: ./pages/patdashboard.php');
-        }
-        else
-        {echo "<script>alert('Not Valid')</script>";
-        }
+    $sql = "SELECT * FROM user WHERE username='$username' AND password='$password' AND Role_id=2";
+    $user = $conn->query($sql);
+    if ($user->num_rows > 0) {
+        $user = $user->fetch_assoc();
+        $_SESSION['id'] =  $user['Id'];
+        $_SESSION['username'] = $user['Username'];
+        $_SESSION['role'] = $user['Role_id'];
+        $_SESSION['name'] = $user['Name'];
+        header('location: ./pages/patdashboard.php');
+    } else {
+        echo "<script>
+            document.addEventListener('DOMContentLoaded', function() {
+                        Swal.fire({
+                        icon: 'error',
+                        title: 'Invalid username or password',
+                        text: 'Please check your credentials and try again.',
+                        confirmButtonText: 'OK', // Customize the button text
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            Swal.close();
+                        }
+                    });
+            });
+            </script>";
     }
+}
 ?>
