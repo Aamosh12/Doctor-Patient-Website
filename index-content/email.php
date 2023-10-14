3c6d053d52946ed6c54e20f2fe0d3adc9f295cbc
Aamosh12 <aamoshmaharjan234@gmail.com>
2023-10-14 13:41:28 +0545

    feat: Added email.php and emailpat.php to check email existence in database

---
+++ index-content/email.php
@@ -0,0 +1,14 @@
+<?php
+require '../actions/Connection.php';
+
+$email = "SELECT * FROM user WHERE email='$_GET[email]' AND Role_id=1";
+$check = $conn->query($email);
+if($check->num_rows >0)
+{
+    echo 'Email Already exist';
+}
+else
+{
+    echo 'Email available';
+}
+?>
